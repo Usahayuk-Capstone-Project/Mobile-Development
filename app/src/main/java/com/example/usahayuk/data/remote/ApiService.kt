ee5699cb8a560c901a5cd4205a596e2a3a0da325
Raihan Israq Zabran <raihanzabrann@gmail.com>
2023-05-30 01:38:20 +0700

    remote login test

--- app/src/main/java/com/example/usahayuk/data/remote/ApiService.kt
+++ app/src/main/java/com/example/usahayuk/data/remote/ApiService.kt
@@ -1,4 +1,27 @@
 package com.example.usahayuk.data.remote
 
+import com.example.usahayuk.data.model.LoginResponse
+import com.example.usahayuk.data.model.RegisterResponse
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
 interface ApiService {
+    @FormUrlEncoded
+    @POST("api/auth/signup")
+    fun register(
+        @Field("displayName") name :String,
+        @Field("email") email :String,
+        @Field("password") password :String
+    ): Call<RegisterResponse>
+
+    @FormUrlEncoded
+    @POST("api/auth/signin")
+    fun login(
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): Call<LoginResponse>
+
+
 }
