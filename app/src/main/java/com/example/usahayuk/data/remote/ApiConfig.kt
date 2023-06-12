be148b0a2a75c6cb91e4b87044ea8a9e1cc0135a
RYanz27 <20102160@ittelkom-pwt.ac.id>
2023-06-12 17:18:48 +0700

    add new api config and update string

--- app/src/main/java/com/example/usahayuk/data/remote/ApiConfig.kt
+++ app/src/main/java/com/example/usahayuk/data/remote/ApiConfig.kt
@@ -7,6 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 import com.example.usahayuk.BuildConfig
 
+private const val MAIN_FEATURE_URL ="https://my-ml-image-vng2lopkba-et.a.run.app/recommendation/"
 class ApiConfig {
     companion object {
         fun getApiService(): ApiService {
@@ -20,6 +21,22 @@ class ApiConfig {
                 .build()
             val retrofit = Retrofit.Builder()
                 .baseUrl(BuildConfig.URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+            return retrofit.create(ApiService::class.java)
+        }
+        fun getApiServiceMainFeature(): ApiService {
+            val loggingInterceptor = if (BuildConfig.DEBUG) {
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            } else {
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+            }
+            val client = OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build()
+            val retrofit = Retrofit.Builder()
+                .baseUrl(MAIN_FEATURE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(client)
                 .build()
